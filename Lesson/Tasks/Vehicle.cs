1ec93ea74455aaa9bf18afbd642c3c2bcfa6b1cb
Aydanshj <121218604+Aydanshj@users.noreply.github.com>
2023-01-30 20:13:59 +0400

    Add files via upload

---
+++ Lesson/Tasks/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_30._01._2023
+{
+    internal class Vehicle
+    {
+        protected double _mileage;
+        public double Mileage => _mileage;
+        public virtual void Drive(double km)
+        {
+            _mileage += km;
+        }
+    }
+}
